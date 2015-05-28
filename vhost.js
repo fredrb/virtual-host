c405ed9dcdfee5c7adae6c7b7efb1c0edefcc0de
Frederico Bittencourt <fred.rbittencourt@gmail.com>
2015-05-28 01:33:12 -0300

    test

--- vhost.js
+++ vhost.js
@@ -4,11 +4,29 @@ var vhost = require('vhost');
 
 var app = connect();
 
+var express = require('express');
+
+var app1 = express();
+app1.get('/', function(req, res){
+  res.end('This is app 1');
+});
+
+var app2 = express();
+app2.get('/', function(req, res){
+  res.end('This is app 2');
+})
+
 // use OS variables to find where the apps are located
 
-app.use(vhost('gba.atcore.co', require(process.env.GBA_APP).app));
-app.use(vhost('chat.atcore.co', require(process.env.CHAT_APP).app));
-app.use(vhost('atcore.co', require(process.env.CHAT_APP).app));
+// app.use(vhost('gba.atcore.co', require(process.env.GBA_APP).app));
+// app.use(vhost('chat.atcore.co', require(process.env.CHAT_APP).app));
+// app.use(vhost('atcore.co', require(process.env.CHAT_APP).app));
+
+
+app.use(vhost('app1.atcore.co', app1));
+app.use(vhost('app2.atcore.co', app2));
+
+app.use(vhost('atcore.co', app1));
 
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
